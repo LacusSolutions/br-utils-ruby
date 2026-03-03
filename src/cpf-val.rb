47ef31470932145cefac6d545099136f4db7955e
juliolmuller <juliolmuller@outlook.com>
2026-03-03 17:15:12 -0300

    style: lint source code

--- src/cpf-val.rb
+++ src/cpf-val.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-require "cpf-dv"
-require_relative "cpf-val/version"
+require 'cpf-dv'
+require_relative 'cpf-val/version'
 
 module CpfVal
   def self.hello
-    "cpf-val"
+    'cpf-val'
   end
 end
--- test/cpf_val_test.rb
+++ test/cpf_val_test.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-require "minitest/autorun"
-require "cpf-val"
+require 'minitest/autorun'
+require 'cpf-val'
 
 class CpfValTest < Minitest::Test
   def test_hello
-    assert_equal "cpf-val", CpfVal.hello
+    assert_equal 'cpf-val', CpfVal.hello
   end
 end
