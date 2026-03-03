bc80166fd42d562130afce2462efa3e71602bcf3
juliolmuller <juliolmuller@outlook.com>
2026-03-03 17:15:12 -0300

    style: lint source code

--- test/cpf_gen_test.rb
+++ test/cpf_gen_test.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
-require "minitest/autorun"
-require "cpf-gen"
+require 'minitest/autorun'
+require 'cpf-gen'
 
 class CpfGenTest < Minitest::Test
   def test_hello
-    assert_equal "cpf-gen", CpfGen.hello
+    assert_equal 'cpf-gen', CpfGen.hello
   end
 end
