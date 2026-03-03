2675f607c5cd263d561ca7681267acf054d35a5a
juliolmuller <juliolmuller@outlook.com>
2026-03-03 15:52:11 -0300

    chore: setup monorepo project

---
+++ src/cnpj-fmt.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "cnpj-fmt/version"
+
+module CnpjFmt
+  def self.hello
+    "cnpj-fmt"
+  end
+end
---
+++ src/cnpj-fmt/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module CnpjFmt
+  VERSION = "0.0.0"
+end
