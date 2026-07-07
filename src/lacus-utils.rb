3dee2bee377c1a0744c4b58ce02ebfbb2c9e9eb0
juliolmuller <juliolmuller@outlook.com>
2026-07-07 13:16:41 -0300

    feat: create new utilities package

---
+++ src/lacus-utils.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative 'lacus-utils/version'
+
+module LacusUtils
+  def self.hello
+    'lacus-utils'
+  end
+end
---
+++ src/lacus-utils/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module LacusUtils
+  VERSION = '0.0.0'
+end
